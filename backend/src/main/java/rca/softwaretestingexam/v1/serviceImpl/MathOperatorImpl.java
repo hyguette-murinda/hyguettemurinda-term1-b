f23538d199f4952cb07844d2699faf6572ec2949
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2023-12-18 09:45:13 +0200

    service Test: addition

--- backend/src/main/java/rca/softwaretestingexam/v1/serviceImpl/MathOperatorImpl.java
+++ backend/src/main/java/rca/softwaretestingexam/v1/serviceImpl/MathOperatorImpl.java
@@ -22,12 +22,6 @@ public class MathOperatorImpl implements IMathOperator {
                 return operand1 + operand2;
             case "-":
                 return operand1 - operand2;
-            case "**":
-                return Math.pow(operand1, operand2);
-            case "log":
-                return operand1 * Math.log10(operand2);
-            case "ln":
-                return operand1 * Math.log(operand2);
             default:
                 throw new RuntimeException("Unknown Operation");
         }
---
+++ backend/src/test/java/rca/softwaretestingexam/service/MathOperatorServiceTest.java
@@ -0,0 +1,40 @@
+package rca.softwaretestingexam.v1.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import rca.softwaretestingexam.v1.dtos.DoMathRequestDto;
+import rca.softwaretestingexam.v1.exceptions.InvalidOperationException;
+import rca.softwaretestingexam.v1.serviceImpl.MathOperatorImpl;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MathOperatorServiceTest {
+
+    @Mock
+    private MathOperatorImpl mathOperatorService;
+//    PcUserService mock = org.mockito.Mockito.mock(PcUserService.class);
+
+    @Test
+    public void whenAdd_returnAddition() throws InvalidOperationException {
+        int operator1 = 3;
+        int operator2 = 1;
+        String operation = "+";
+
+        double mathOperation = mathOperatorService.doMath(operator1,operator2, operation);
+        when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(mathOperation);
+
+        double actualMathOperator = mathOperatorService.doMath(operator1, operator2, operation);
+
+        assertThat(mathOperation).isEqualTo(actualMathOperator);
+    }
+
+    
+   
+
+}
